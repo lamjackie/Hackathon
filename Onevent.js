3570316c137a174e5e107c325f3c20896ae14e39
XinTongByte <116852622+XinTongByte@users.noreply.github.com>
2023-02-18 15:28:16 -0800

    Update Onevent.js

--- Onevent.js
+++ Onevent.js
@@ -3,3 +3,10 @@ var plus;
 onClick(plus, "click", function(event){
   ();
 });
+    <header class="nav-bar">
+           <a ref="index.html" class="logo">
+           <a class="scroll1" href="#about">About</a>
+           <a class="scroll2" href="hours.html">Service Hours</a>
+           <a class="scroll2" href="#credits">Credits</a>
+           <a class="scroll2" href="index.html">Home</a>
+    </header>
